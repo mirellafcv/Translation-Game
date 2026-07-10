0d39658932d001868400ce99ce214a4e9d11f805
mirellafcv <mirellafcv@gmail.com>
2026-07-10 16:32:17 -0300

    Add files via upload

---
+++ jogo traducao.py
@@ -0,0 +1,91 @@
+import sounddevice as sd
+import numpy as np
+import scipy.io.wavfile as wav
+import speech_recognition as sr
+from googletrans import Translator
+import random
+
+
+vidas = 3  # número de vidas do jogador
+
+while True:
+
+    duration = 5  # segundos de gravação
+    sample_rate = 44100
+    words_by_level = {
+        "fácil": ["gato", "pássaro", "pão", "leite", "sol"],
+        "médio": ["casa", "escola", "amigo", "janela", "amarelo"],
+        "difícil": ["tecnologia", "universidade", "informação", "pronúncia", "passaporte"]
+    }
+
+    level = input("Escolha o nível de dificuldade (fácil, médio, difícil) ou 'sair' para encerrar: ").lower()
+    if level == "sair":
+        break
+    lang = input("Digite o código do idioma para traduzir ('ro' para romeno ou 'en' para inglês): ").lower()
+    palavra = random.choice(words_by_level[level])
+
+    translator = Translator()
+    gabarito = translator.translate(palavra, dest=lang,  src = 'pt') #??????
+    print(palavra)
+
+    print("Fale agora...")
+    recording = sd.rec(
+    int(duration * sample_rate), # o número de amostras a serem registradas
+    samplerate=sample_rate,      # taxa de amostras
+    channels=1,                  # 1 significa gravação mono
+    dtype="int16")               # tipo de dados para as amostras registradas
+    sd.wait()  # aguardando o término da gravação
+
+    wav.write("output.wav", sample_rate, recording)
+    print("Gravação concluída, estou reconhecendo...")
+
+    recognizer = sr.Recognizer()
+    with sr.AudioFile("output.wav") as source:
+        audio = recognizer.record(source)
+
+    try:
+        text = recognizer.recognize_google(audio, language=lang)
+        print("Você disse:", text)
+        if text.lower() == gabarito.text:
+            print("✅ Parabéns! Você acertou a palavra!")
+        else:
+            print("❌ Que pena! A palavra correta era:", gabarito.text)
+            vidas -= 1
+            print(f"Você tem {vidas} vidas restantes.")
+            if vidas == 0:
+                print('''💀 Game Over! Você perdeu todas as suas vidas. \n ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁
+⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀
+⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀
+⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀
+⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀
+⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀
+⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆
+⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀
+⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀
+⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀
+⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀
+⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀''')
+                break
+    except sr.UnknownValueError:             # - se o Google não conseguiu entender a fala devido a ruídos ou silêncio
+        print("A fala não pôde ser reconhecida.")
+    except sr.RequestError as e:             # - se não houver conexão com a Internet ou a API estiver indisponível
+        print(f"Service error: {e}")
+
+
+
+
+
+
+
+'''recognized = recognized.lower()''' #onde botar?
+
+
+'''
+translator = Translator()
+translated = translator.translate(text, dest=lang)  # O 'ro' aqui é um código para romeno
+print(f"🌍 Tradução para o {lang}:", translated.text)
+'''
